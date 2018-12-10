b3fa1026975fd790319b5c5428f6b488ace771ef
HappyMell <mellisamadrid@hotmail.com>
2018-12-10 16:42:52 +0000

    Updated animations and ES6

--- css/app.js
+++ css/app.js
@@ -4,24 +4,27 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
-        turns: []
+        turns: [],
+        currentTurn: 0
     },
     methods: {
-        startGame: function(){
+        startGame(){
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.turns = [];
         },
 
-        attack: function() {
+        attack() {
             var damage = this.calculateDamage(3, 10);
             this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: `Player hits monster for ${damage}`
+                text: `Player hits monster for ${damage}`,
+                id: this.currentTurn + 1
             });
 
+            this.currentTurn++;
             if(this.checkWin()) {
                 return;
             }
@@ -29,40 +32,41 @@ new Vue({
            this.monsterAttacks();
         }, //end attack method
 
-        specialAttack: function() {
+        specialAttack() {
             var damage = this.calculateDamage(10, 20);
             this.monsterHealth -= damage;
 
             this.turns.unshift({
                 isPlayer: true,
-                text: `Player uses special attack for ${damage}`
+                text: `Player uses special attack for ${damage}`,
+                id: this.currentTurn + 1
             });
-
+            this.currentTurn++;
             if(this.checkWin()) {
                 return;
             }
             this.monsterAttacks();
          
         }, // end special attack method
-        heal: function(){
+        heal(){
             if (this.playerHealth <= 90) {
-                this.playerHealth += 5;
+                this.playerHealth += 10;
             } else {
                 this.playerHealth = 100;
             }
 
-
-            this.playerHealth += 5;
-            this.monsterAttacks();
-
-            this.turns.unshift({
+                this.turns.unshift({
                 isPlayer: true,
-                text: `Player heals for 5`
+                text: `Player heals for 5`,
+                id: this.currentTurn + 1
             });
+
+            this.currentTurn++
+            this.monsterAttacks();
 
         }, //end heal method
 
-        giveUp: function(){
+        giveUp(){
             if(confirm("You have given up. Play again?")) {
                 this.startGame();
             } else {
@@ -73,27 +77,30 @@ new Vue({
 
             this.turns.unshift({
                 isPlayer: true,
-                text: `Player has given up.`
+                text: `Player has given up.`,
+                id: this.currentTurn + 1
             });
 
         }, //end give up method
 
-        monsterAttacks: function() {
+        monsterAttacks() {
             var damage = this.calculateDamage(5, 12);
             this.playerHealth-= damage;
             this.checkWin();
             this.turns.unshift({
                 isPlayer: false,
-                text: `Monster hits player for ${damage}`
+                text: `Monster hits player for ${damage}`,
+                id: this.currentTurn + 1
             })
+            this.currentTurn++;
 
         }, //end monster attacks
         
-        calculateDamage: function(min, max){
+        calculateDamage(min, max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         }, //end calculate damage method
 
-        checkWin: function(){
+        checkWin(){
             if(this.monsterHealth <= 0) {
 
                 if(confirm("You won! Play again?")) {
